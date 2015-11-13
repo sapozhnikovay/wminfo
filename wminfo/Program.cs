3e70d6727c694a928c485ad4b7d0bedaae523dc3
fessxxl <fessxxl@users.noreply.github.com>
2015-11-13 15:45:33 +0300

    Versioning and help

--- wminfo/Program.cs
+++ wminfo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,57 @@ namespace wminfo
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                
+                return 1;
+            }
+            else
+            {
+                ParseArguments(args);
+            }
+
+            Console.ReadKey();
+            return 0;
+        }
+
+        static void DisplayHelp()
+        {
+            // TODO: Add help display
+            Console.WriteLine("Usage: wminfo [OPTIONS]...");
+            Console.WriteLine("Get information about local or remote computer via WMI.");
+            Console.WriteLine("Query information from local computer and display summary on screeen if no options specified.\n");
+            Console.WriteLine("  --help\tdisplay this help and exit");
+            Console.WriteLine("  --version\toutput version information and exit");
+        }
+
+        static void DisplayVersion()
+        {
+            //TODO: Display application version
+            Console.WriteLine("wminfo "+ Assembly.GetExecutingAssembly().GetName().Version.ToString());
+        }
+
+        static void ParseArguments(string[] args)
+        {
+            foreach(string arg in args)
+            {
+                switch (arg)
+                {
+                    case "-h":
+                        DisplayHelp();
+                        break;
+                    case "--help":
+                        DisplayHelp();
+                        break;
+                    case "--version":
+                        DisplayVersion();
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
--- wminfo/Properties/AssemblyInfo.cs
+++ wminfo/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.4.1244")]
+[assembly: AssemblyFileVersion("0.0.4.1244")]
